5643ace190e5d309391cb25e5fed3722c5a7e9ce
TakWolf <takwolf@foxmail.com>
2026-07-22 10:44:16 +0800

    调整 dump 函数参数

--- tools/build.py
+++ tools/build.py
@@ -1,6 +1,5 @@
 import shutil
 
-from tools import configs
 from tools.configs import path_define
 from tools.services import setup_service, dump_service, font_service, publish_service
 
@@ -11,11 +10,8 @@ def main():
 
     setup_service.setup_ark_pixel()
 
-    for dump_config in configs.dump_configs:
-        dump_service.dump_font(dump_config)
-
-    for fallback_config in configs.fallback_configs:
-        dump_service.apply_fallback(fallback_config)
+    dump_service.dump_fonts()
+    dump_service.apply_fallbacks()
 
     contexts_12 = font_service.load_contexts(12)
     font_service.make_fonts(12, 'Normal', contexts_12, False)
