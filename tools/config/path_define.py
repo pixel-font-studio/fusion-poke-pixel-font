b941e8ffda4bd7a777a4222061212dc8d38cb0ca
TakWolf <takwolf@foxmail.com>
2026-09-26 13:50:02 +0800

    提取字形校验规则为配置文件

--- tools/config/path_define.py
+++ tools/config/path_define.py
@@ -6,6 +6,7 @@ ASSETS_DIR = PROJECT_ROOT_DIR.joinpath('assets')
 
 CONFIGS_DIR = ASSETS_DIR.joinpath('configs')
 CONFIGS_FONTS_DIR = CONFIGS_DIR.joinpath('fonts')
+CONFIGS_GLYPHS_DIR = CONFIGS_DIR.joinpath('glyphs')
 CONFIGS_MAPPINGS_DIR = CONFIGS_DIR.joinpath('mappings')
 
 FONTS_DIR = ASSETS_DIR.joinpath('fonts')
